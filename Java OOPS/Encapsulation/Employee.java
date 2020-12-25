bbe3f6e6e4314dc6ee7c92c85ce013645c198951
himanshu <bhimanshu031@gmail.com>
2020-12-25 19:30:16 +0530

    Understanding the concept of the Encapsulation

---
+++ Java OOPS/Encapsulation/Employee.java
@@ -0,0 +1,14 @@
+class Employee
+{
+    private int empid;      // data hiding
+
+    public void setEmpId(int id)
+    {
+        empid = id;
+    }
+
+    public int getEmpId()
+    {
+        return empid;
+    }
+}
---
+++ Java OOPS/Encapsulation/Test.java
@@ -0,0 +1,8 @@
+class Test
+{
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        e.setEmpId(10);
+        System.out.println(e.getEmpId());
+    }
+}
