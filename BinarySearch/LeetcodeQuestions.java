b3c929da946886847c2548a8c6107ec1cb02c30e
himanshu <bhimanshu031@gmail.com>
2021-01-04 02:05:49 +0530

    Some More Leetcode Questions on Binary Search

--- BinarySearch/LeetcodeQuestions.java
+++ BinarySearch/LeetcodeQuestions.java
@@ -7,6 +7,14 @@ public class LeetcodeQuestions
         // LEETCODE - 367 , VALID PERFECT SQUARE 
 
         // LEETCODE - 278 , FIRST BAD VERSION
+
+        // LEETCODE - 1011 , CAPACITY TO SHIP PACKAGE WITHIN D DAYS 
+
+        // LEETCODE - 875 , KOKO EATING BANANAS
+
+        // LEETCODE - 852, Peak Index in a Mountain Array
+
+        // leetcode - 1351, Count Negative Numbers in a Sorted Matrix
     }
 
     // LEETCODE -794 , BINARY SEARCH ____________________________________________________
@@ -87,5 +95,168 @@ public class LeetcodeQuestions
         }
         
         return ans;
+    }
+
+
+    // LEETCODE - 1011 , CAPACITY TO SHIP PACKAGE WITHIN D DAYS 
+    public int shipWithinDays(int[] weights, int D)
+    {
+        int max = 0;
+        int sum = 0;
+        for(int ele : weights)
+        {
+            max = Math.max(max , ele);
+            sum += ele;
+        }
+        
+        int l = max;
+        int r = sum;
+        
+        int ans = r;
+        
+        while(l <= r)
+        {
+            int mid = l + (r - l) / 2;  // possible ans
+            
+            // find the Days required for this minimum weight
+            int reqDays = 1;
+            int sumWt = 0;
+            for(int i = 0 ; i < weights.length ; i++)
+            {
+                if(sumWt + weights[i] > mid)
+                {
+                    reqDays++;
+                    sumWt = 0;
+                }
+                sumWt += weights[i];
+            }
+            
+            if(reqDays <= D)
+            {
+                ans = mid;
+                r = mid - 1;        // as there is a possiblity to find lower weight capacity
+            }
+            else
+            {
+                l = mid + 1;      // discard left part as lower could not be the answer
+            }
+        }
+        
+        
+        return ans;
+    }
+
+
+    // LEETCODE - 875 , KOKO EATING BANANAS ______________________________________________________
+    public int minEatingSpeed(int[] piles, int H) 
+    {
+        int max = 0;
+        for(int ele : piles)
+        {
+            max = Math.max(ele , max);
+        }
+        
+        int l = 1;
+        int r = max;
+        
+        int ans = r;
+        
+        while(l <= r)
+        {
+            int mid = l + (r - l) / 2;  // possible ans
+            
+            // required hours for this mid
+            int reqHours = 0;
+            for(int i = 0 ; i < piles.length ; i++)
+            {
+                int q = piles[i] / mid;
+                int rem = piles[i] % mid;
+                
+                reqHours = reqHours + (q + ((rem == 0) ? 0 : 1));
+            }
+            
+            
+            if(reqHours <= H)
+            {
+                ans = mid;
+                r = mid - 1;        // as there is a possibility of lower rate than this
+            }
+            else
+            {
+                l = mid + 1;    // discard lower values as there is no chance for lower values
+            }
+        }
+        
+        return ans;
+    }
+
+
+    // LEETCODE - 852, Peak Index in a Mountain Array _____________________________________________
+    public int peakIndexInMountainArray(int[] arr) 
+    {
+        int l = 0;
+        int r = arr.length - 1;
+        
+        while(l <= r)
+        {
+            int mid = l + (r - l) / 2;
+            
+            if(mid != 0 && mid != arr.length - 1 && 
+               arr[mid] > arr[mid-1] && arr[mid] > arr[mid + 1])
+            {
+                return mid;
+            }
+            else if(mid == arr.length - 1 || arr[mid] > arr[mid + 1])
+            {
+                r = mid -1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+
+    // leetcode - 1351, Count Negative Numbers in a Sorted Matrix _________________________________
+    public int countNegatives(int[][] grid) 
+    {
+        int ans = 0;
+        for(int i = 0 ; i < grid.length ; i++)
+        {
+            ans += calculate(grid[i]);
+        }
+        
+        return ans;
+    }
+    
+    public int calculate(int[] arr)
+    {
+        int l = 0;
+        int r = arr.length - 1;
+        
+        while(l <= r)
+        {
+            int mid = l + (r - l) / 2;
+            
+            if(arr[mid] < 0)
+            {
+                if(mid - 1 >= 0 && arr[mid - 1] < 0)
+                {
+                    r = mid - 1;
+                }
+                else
+                {
+                    return arr.length - mid;
+                }
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        
+        return 0;
     }
 }
